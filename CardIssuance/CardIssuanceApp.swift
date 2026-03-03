43c37c9e843e457f1e10735163f6ea836260af6b
sergeybockow <theserega234@gmail.com>
2026-03-03 17:02:16 +0300

    Initial Commit

---
+++ CardIssuance/CardIssuanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardIssuanceApp.swift
+//  CardIssuance
+//
+//  Created by Сергей Бочков on 03.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct CardIssuanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
